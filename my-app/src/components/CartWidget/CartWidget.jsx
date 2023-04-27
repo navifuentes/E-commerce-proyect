1f476cfb5e34bd9a2d2ee8cf7ae237def232f5e2
Ivan Fuentes <iv.ifa94@gmail.com>
2023-04-27 00:13:02 -0400

    cartWidget

---
+++ my-app/src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,19 @@
+import { React, useState } from "react";
+
+const CartWidget = () => {
+  const [numero, setNumero] = useState(0);
+
+  const sumar = () => {
+    setNumero(numero + 1);
+  };
+  const restar = () => {
+    setNumero(numero - 1);
+  };
+  const reset = () => {
+    setNumero(0);
+  };
+
+  return <div>&#128722;{numero} </div>;
+};
+
+export default CartWidget;
--- my-app/src/components/Navbar/Navbar.js
+++ my-app/src/components/Navbar/Navbar.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Logo from "./Logo/Logo.js";
 import Categorias from "./Categorias/Categorias.js";
-import PerfilIcon from "./PerfilIcon/PerfilIcon.js";
 import HamburgerMenu from "./HamburgerMenu/HamburgerMenu.js";
+import CartWidget from "../CartWidget/CartWidget.jsx";
 
 const Navbar = () => {
   return (
     <nav className="sticky h-1/4 flex flex-row justify-between mx-auto top-0 z-10 bg-blue-200 dark:bg-blue-950">
       <Logo />
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-row">
         <Categorias />
         <HamburgerMenu />
-        <PerfilIcon />
+        <CartWidget />
       </div>
     </nav>
   );
